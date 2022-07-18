208e1ff7350f324ec686c2d3eb1c7e56f60ba6a2
withzeal <withzeal@163.com>
2022-07-18 22:09:12 +0800

    重新支持自定义日志输出方式；解决父进程在子进程启动后不处理旧请求就退出的问题。

--- restart.go
+++ restart.go
@@ -34,22 +34,25 @@ func (ser *restartServer) Run() (err error) {
 		return
 	}
 
-	go handlerSignal(ser.listener, ser.server, afterTask)
-
-	if len(beforeTask) != 0 {
-		for _, task := range beforeTask {
-			if task.isGo {
-				go task.task()
-			} else {
-				task.task()
+	go func() {
+		if len(beforeTask) != 0 {
+			for _, task := range beforeTask {
+				if task.isGo {
+					go task.task()
+				} else {
+					task.task()
+				}
 			}
 		}
-	}
 
-	err = ser.server.Serve(ser.listener)
-	if err != nil && err != http.ErrServerClosed {
-		logger.Println(err)
-	}
+		err = ser.server.Serve(ser.listener)
+		if err != nil && err != http.ErrServerClosed {
+			logger.Println(err)
+			panic(err)
+		}
+	}()
+
+	handlerSignal(ser.listener, ser.server, afterTask)
 	return
 }
 
@@ -66,23 +69,25 @@ func (ser *restartServer) RunTLS(certPath, keyPath string) (err error) {
 		return
 	}
 
-	go handlerSignal(ser.listener, ser.server, afterTask)
-
-	if len(beforeTask) != 0 {
-		for _, task := range beforeTask {
-			if task.isGo {
-				go task.task()
-			} else {
-				task.task()
+	go func() {
+		if len(beforeTask) != 0 {
+			for _, task := range beforeTask {
+				if task.isGo {
+					go task.task()
+				} else {
+					task.task()
+				}
 			}
 		}
-	}
 
-	err = ser.server.ServeTLS(ser.listener, certPath, keyPath)
-	if err != nil && err != http.ErrServerClosed {
-		logger.Println(err)
-		return
-	}
+		err = ser.server.ServeTLS(ser.listener, certPath, keyPath)
+		if err != nil && err != http.ErrServerClosed {
+			logger.Println(err)
+			panic(err)
+			return
+		}
+	}()
 
+	handlerSignal(ser.listener, ser.server, afterTask)
 	return
 }
